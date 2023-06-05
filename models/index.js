a09172ded791715cd823b09e4e381fb1a1a1fc81
saixrr <111263091+saixrr@users.noreply.github.com>
2023-06-05 16:12:30 +0530

    Update index.js

--- models/index.js
+++ models/index.js
@@ -13,7 +13,11 @@ let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+  sequelize = new Sequelize(config.database, config.username, config.password, {
+    host: config.host,
+    dialect: config.dialect,
+    // Add any other necessary configuration options here
+  });
 }
 
 fs
