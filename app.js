c4b840cb974da3710b71f0e3d2bb24d74a20511f
saixrr <111263091+saixrr@users.noreply.github.com>
2023-06-11 02:01:14 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const flash = require("connect-flash");
 const express = require('express');
 var csrf = require("tiny-csrf");
 const app = express();
-const { Sport,User,Session } = require("./models");
+const { Sport,User,Session,SessionPlayer } = require("./models");
 const path = require('path');
 const bodyParser = require("body-parser");
 var cookieParser = require("cookie-parser");
@@ -16,10 +16,10 @@ const session = require('express-session');
 const LocalStartegy=require('passport-local');
 const bcrypt = require('bcrypt');
 const { request } = require("http");
-const { user } = require("pg/lib/defaults");
+const { user, password } = require("pg/lib/defaults");
 const saltRounds=10;
 const moment = require("moment");
-const { where } = require("sequelize");
+const { where,Op ,Sequelize} = require("sequelize");
 app.use(cookieParser("shh!some secret string"));
 app.use(csrf("this_should_be_32_character_long",["POST","PUT","DELETE"]));
 
@@ -34,8 +34,6 @@ app.use(session({
   }
 }))
 ;
-app.use(passport.initialize());
-app.use(passport.session());
 
 app.set("view engine", "ejs");
 app.use(flash());
@@ -43,6 +41,8 @@ app.use(function(request, response, next) {
   response.locals.messages = request.flash();
   next();
 });
+app.use(passport.initialize());
+app.use(passport.session());
 const getUserIdMiddleware = (req, res, next) => {
   if (req.user) {
     req.userId = req.user.id;
@@ -111,7 +111,7 @@ app.get('/', (req, res) => {
     try {
       const userId = req.user.id;
       const sports = await Sport.findAll({ where: { userId } });
-      res.render('create-sport.ejs', { sports,csrfToken: req.csrfToken(), });
+      res.render('create-sport.ejs', { sports,successMessage: req.flash('successMessage'), errorMessage: req.flash('errorMessage') ,csrfToken: req.csrfToken(), });
     } catch (error) {
       console.log(error);
       res.status(404).send("Error retrieving sports");
@@ -119,28 +119,31 @@ app.get('/', (req, res) => {
   });
   
 
-app.post("/create-session", async (req, res) => {
-  const sportName=req.body.sportName;
-  console.log(sportName);
-  const userId=req.userId;
-  console.log(userId)
-  try {
-    const sport = await Sport.createNewSport(userId,sportName);
-    res.redirect("/create-session");
-  } catch (error) {
-    console.log(error);
-    res.status(404).send("Sport not found");
-  }
-});
-app.get("/newsession/:sportId/:sportName", (req, res) => {
+  app.post('/create-session',connectEnsureLogin.ensureLoggedIn(), async (req, res) => {
+    try {
+      const userId = req.user.id;
+      const name = req.body.sportName.toLowerCase();
+  
+      await Sport.createNewSport(userId, name);
+  
+      res.locals.messages = req.flash("success", "Sport successfully created");
+      res.redirect('/create-session');
+    } catch (error) {
+      res.locals.messages = req.flash("error", error.message);
+      res.redirect('/create-session');
+    }
+  });
+  
+app.get("/newsession/:sportId/:sportName",connectEnsureLogin.ensureLoggedIn(), async (req, res) => {
   const sportId = req.params.sportId;
   console.log(sportId)
   const sportName = req.params.sportName;
+  const users=await User.findAll()
   console.log(sportName)
-  res.render("sport-details.ejs", { sportId,sportName,csrfToken: req.csrfToken(), });
+  res.render("sport-details.ejs", { sportId,sportName,users,csrfToken: req.csrfToken(), });
 });
 
-app.post("/newsession", async (req, res) => { 
+app.post("/newsession",connectEnsureLogin.ensureLoggedIn(), async (req, res) => { 
   const sportId = req.body.sportId;
   console.log(sportId);
   sportName=req.body.sportName;
@@ -158,13 +161,13 @@ app.post("/newsession", async (req, res) => {
       remaining
     }, Number(sportId), sportName);
     console.log(session.id)
-    res.redirect("/session/"+session.id);
+    res.redirect("/sports");
   }catch (error) {
     console.log(error);
     res.status(404).send("Error creating session");
   }
 });
-app.get("/session/:sessionid", async (req, res) => {
+app.get("/session/:sessionid",connectEnsureLogin.ensureLoggedIn(), async (req, res) => {
   const sessionId = req.params.sessionid;
   try {
     const session = await Session.findByPk(sessionId);
@@ -238,7 +241,7 @@ app.get("/session/:sessionid", async (req, res) => {
     
 
 
-app.get("/sessions/:sessionid", async (req, res) => {
+app.get("/sessions/:sessionid",connectEnsureLogin.ensureLoggedIn(), async (req, res) => {
   const sessionId = req.params.id;
   const session = await Session.getSessionById(sessionId);
   res.render("session-details.ejs", {
@@ -257,14 +260,41 @@ app.get("/sessions/:sessionid", async (req, res) => {
 
    app.get("/sports/:sportName",connectEnsureLogin.ensureLoggedIn(),async (req,res)=>{
     const name=req.params.sportName
-    const sessions=await Session.findAll({where:{
-      sportName:name
-    }
-    })
-    console.log(sessions)
-    res.render("sportsessions.ejs",{sessions,})
+    const sessions=await Session.getlatestSessions(name)
+    const message=req.flash('message');
+    res.render("sportsessions.ejs",{sessions,name,message})
    })
+app.get("/joinedsessions",connectEnsureLogin.ensureLoggedIn(),async(req,res)=>{
+  try{
+    const userId=req.userId;
+    const user=await User.findOne({where:{id:userId}})
+    const email=user.email
+    const sessions=await Session.getJoinedSessions(email);
+    res.render("joinedsessions.ejs",{sessions})
+  }catch(error){
+    console.error("error while displaying the joined sessions: ",error)
+    res.status(500).send("An error while displaying the joinedsessions")
+  }
 
+})
+app.get('/leavesession/:sessionId',connectEnsureLogin.ensureLoggedIn(),async(req,res)=>{
+  const sessionid=req.params.sessionId;
+ try{
+  const userId=req.userId;
+  const sessionplayer=await Session.findOne({where:{id: sessionid}});
+  const user=await User.findOne({where:{id:userId}})
+  await SessionPlayer.destroy({where:{
+    sessionId:sessionplayer.id,
+  }
+  })
+  await Session.leaveSession(user.email,sessionid);
+  res.redirect("/sports")
+ }catch(error){
+  console.log("error while leaving a session:",error)
+  res.status(500).send("An error occured while leaving a session")
+ }
+
+})
     
 
 app.get("/sports",connectEnsureLogin.ensureLoggedIn(), async (req, res) => {
@@ -273,8 +303,33 @@ app.get("/sports",connectEnsureLogin.ensureLoggedIn(), async (req, res) => {
     sports: sports,
   });
 });
+app.get("/usersessions",connectEnsureLogin.ensureLoggedIn(), async (req, res) => {
+  const userId=req.userId;
+  const sessions = await Session.findAll({where:{creatorId:userId}});
+  res.render("usersessions.ejs", {sessions});
+});
+app.get('/cancelsession/:sessionId', async (req, res) => {
+  const sessionId=req.params.sessionId
+  try {
+    res.render('cancelreason.ejs',{sessionId,csrfToken:req.csrfToken()});
+  } catch (error) {
+    console.error('Error getiing reason for cancellation:', error);
+    res.status(500).send('An error occurred while getiing reason for cancellation');
+  }
+});
+app.post('/cancelsession/:sessionId/confirm',connectEnsureLogin.ensureLoggedIn(), async (req, res) => {
+  const sessionId=req.params.sessionId;
+  const reason = req.body.reason
+  try {
+    await Session.cancelSession(sessionId,reason);
+    res.redirect('/usersessions');
+  } catch (error) {
+    console.error('Error cancelling Session:', error);
+    res.status(500).send('An error occurred while cancelling the Session');
+  }
+});
 
-app.get('/delete-sport', async (req, res) => {
+app.get('/delete-sport',connectEnsureLogin.ensureLoggedIn(), async (req, res) => {
   try {
     const sportName = req.query.sport;
     const sport = await Sport.findOne({ where: { name: sportName } });
@@ -290,6 +345,113 @@ app.get('/delete-sport', async (req, res) => {
     res.status(500).send('An error occurred while deleting the sport');
   }
 });
+app.get('/sessionplayer/:sessionId',connectEnsureLogin.ensureLoggedIn(),async(req,res)=>{
+  const sessionid=req.params.sessionId;
+ try{
+  const userId=req.userId;
+  const sessionplayer=await Session.findOne({where:{id: sessionid}});
+  const user=await User.findOne({where:{id:userId}})
+  await SessionPlayer.create({
+    sessionId:sessionplayer.id,
+    playername:user.firstName + user.lastName,
+    userId:userId
+  })
+  await Session.joinSession(user.email,sessionid);
+  res.redirect("/joinedsessions")
+ }catch(error){
+  console.log("error while joining a session:",error)
+  res.status(500).send("An error occured while joining a session")
+ }
+
+});
+app.get('/reports',connectEnsureLogin.ensureLoggedIn(),async(req,res)=>{
+  res.render("report.ejs")
+})
+
+async function getReportData(startDate, endDate) {
+  try {
+    const sessionsCountQuery = await Session.findAll({
+      where: {
+        date: {
+          [Op.gte]: startDate, // Greater than or equal to startDate
+          [Op.lte]: endDate,   // Less than or equal to endDate
+        }
+      }
+    });
+    console.log(sessionsCountQuery)
+
+    const sessionsCount = sessionsCountQuery.length;
+    console.log(sessionsCount)
+
+    const sportsPopularityQuery = await Session.findAll({
+      attributes: ['sportName', [Sequelize.fn('COUNT', Sequelize.col('sportName')), 'popularity']],
+      where: {
+        date: {
+          [Op.gte]: startDate,
+          [Op.lte]: endDate,   
+        }
+      },
+      group: ['sportName']
+    });
+    console.log(sportsPopularityQuery)
+
+    const sportsPopularity = {};
+    for (const row of sportsPopularityQuery) {
+      sportsPopularity[row.sportName] = row.get('popularity');
+    }
+
+    return { sessionsCount, sportsPopularity };
+  } catch (error) {
+    console.log(error)
+    throw new Error('Error retrieving report data');
+  }
+}
+
+
+app.get('/api/reports',connectEnsureLogin.ensureLoggedIn(), async (req, res) => {
+  const startDate = req.query.startDate;
+  const endDate = req.query.endDate;
+
+  try {
+    const reportData = await getReportData(startDate, endDate);
+    res.json(reportData);
+  } catch (error) {
+    console.error('Error retrieving report data:', error);
+    res.status(500).json({ error: 'Error retrieving report data' });
+  }
+});
+app.get('/edit-profile',connectEnsureLogin.ensureLoggedIn(),async(req,res)=>{
+  userId=req.userId;
+  const user=await User.findOne({where:{id:userId}})
+  res.render("edit-profile.ejs",{user,csrfToken:req.csrfToken()})
+})
+app.post('/edit-profile', connectEnsureLogin.ensureLoggedIn(), async (req, res) => {
+  const userId = req.userId;
+
+  try {
+    const user = await User.findOne({ where: { id: userId } });
+    const isPasswordMatch = await bcrypt.compare(req.body.password, user.password);
+    if (isPasswordMatch) {
+      res.locals.messages = req.flash("error", "Old and New Passwords are the same");
+      return res.redirect("/edit-profile");
+    }
+
+    const newpassword = await bcrypt.hash(req.body.password, saltRounds);
+    const newfirstName = req.body.firstName;
+    const newlastName = req.body.lastName;
+    const newemail = req.body.email;
+
+    await User.updateuserbyId(userId, newfirstName, newlastName, newemail, newpassword);
+
+    res.locals.messages = req.flash("success", "Profile successfully updated");
+    res.redirect("/sessions");
+  } catch (error) {
+    console.log(error);
+    res.locals.messages = req.flash("error", error.message);
+    res.redirect('/edit-profile');
+  }
+});
+
 
 
 
